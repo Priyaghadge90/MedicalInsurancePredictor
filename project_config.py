c3fd56f8f30de36dc13f8874d59c3edb06083c77
Priya Abhang <priyaghadge90@gmail.com>
2025-05-01 17:55:17 +0530

    added all file path

---
+++ project_config.py
@@ -0,0 +1,7 @@
+CSV_FILE_PATH=r"C:\priya python\python learning\linear regression\medical_prediction\MedicalInsurancePredictor\Data\medical_insurance.csv"
+
+MODEL_FILE_PATH=r"C:\priya python\python learning\linear regression\medical_prediction\MedicalInsurancePredictor\Artifacts\lin_reg_model.pkl"
+
+Label_encoded_data_path=r'C:\priya python\python learning\linear regression\medical_prediction\MedicalInsurancePredictor\Artifacts\lab_enc_code.json'
+
+FLASK_PORT_NUMBER=5005
